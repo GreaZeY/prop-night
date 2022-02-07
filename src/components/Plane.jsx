3bb5373229820b95b94f42dd314e2dda4dce4d91
GreaZeY <greaserevive@gmail.com>
2022-02-08 03:24:22 +0530

    added running anims

--- src/components/Plane.jsx
+++ src/components/Plane.jsx
@@ -2,9 +2,9 @@
 const Plane = () => {
 
     return (
-      <mesh rotation-x={-Math.PI/2} receiveShadow >
-        <planeBufferGeometry attach='geometry' args={[5000, 5000]} />
-        <meshPhysicalMaterial attach='material'  color={'rgb(240, 240, 240)'} />
+      <mesh rotation-x={-Math.PI/2} position={[0,-1,0]} receiveShadow >
+        <planeBufferGeometry attach='geometry' args={[500, 500]} />
+        <meshPhysicalMaterial attach='material' opacity={.4}  color='rgb(240, 240, 240)' />
       </mesh>
     )
 };
--- src/components/playerModel/Player.jsx
+++ src/components/playerModel/Player.jsx
@@ -4,58 +4,57 @@ import { FBXLoader } from "three/examples/jsm/loaders/FBXLoader";
 // import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader'
 import * as THREE from "three";
 import { useEffect, useRef, useState } from "react";
-
 const Player = () => {
+  const [anim, setAnim] = useState(0); //current animation index
+  const player = useRef();
+  const fbx = useLoader(FBXLoader, "/models/fbx/players/idle.fbx");
+  const anims = ["Running"]; // defining all animations
+  const runningAnim = useLoader(
+    FBXLoader,
+    `/models/fbx/players/${anims[0]}.fbx`
+  ); // loading animations
 
-    const [anim,setAnim]=useState(0) //current animation index
-    const player=useRef()
-    const fbx = useLoader(FBXLoader, "/models/fbx/players/idle.fbx");
-    const anims = ["Running"]; // defining all animations
-    const runningAnim = useLoader(
-        FBXLoader,
-        `/models/fbx/players/${anims[0]}.fbx`
-    ); // loading animations
-
-  
   let mixer;
+
+  useEffect(() => {
+    mixer = new THREE.AnimationMixer(fbx);
+
+    const action = mixer.clipAction(fbx.animations[anim]);
+    action.play();
+  }, [anim]);
 
   useEffect(() => {
     fbx.animations.push(runningAnim.animations[0]);
     fbx.receiveShadow = true;
     fbx.castShadow = true;
-    // fbx.scale.set(0.5, 0.5, 0.5);
+    fbx.scale.set(0.5, 0.5, 0.5);
     console.log(fbx);
+    player.current.position.z = -250;
 
-    if (fbx.animations.length) {
-      mixer = new THREE.AnimationMixer(fbx);
-
-      const action = mixer.clipAction(fbx.animations[anim]);
-      action.play();
-    }
-
-  }, [anim]);
-
-  useEffect(()=>{
-    document.addEventListener("keyup", (e)=>{
-        if(e.code!=='KeyW') return
-        console.log('up')
-        setAnim(0)
+    document.addEventListener("keyup", (e) => {
+      console.log("up");
+      setAnim(0);
     });
-  
-    document.addEventListener("keydown", (e)=>{
-      if(e.code!=='KeyW') return
-      console.log('down')
-      setAnim(1)
-      console.log(player.current.position.x)
-      player.current.position.z+=1
-  });
-  },[])
+
+    document.addEventListener("keydown", (e) => {
+      console.log(e);
+      switch (e.code) {
+        case "ArrowUp":
+          setAnim(1);
+          break;
+      }
+    });
+  }, []);
 
   useFrame((state, delta) => {
     mixer?.update(delta);
+
+    if (anim) player.current.position.z += 1;
   });
 
-  return <primitive ref={player} object={fbx} />;
+  return (
+      <primitive ref={player} object={fbx} />
+  );
 };
 
 export default Player;
